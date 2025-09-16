ccd9a2861f868de789db364ba2051814b54388aa
NullCyan <gabrielthejota@gmail.com>
2025-09-16 15:02:29 -0300

    create platform check util

---
+++ Source/Common/Utils/PlatformCheck.cs
@@ -0,0 +1,17 @@
+using Godot;
+namespace NullCyan.Util;
+
+public static class PlatformCheck
+{
+    public static string OsName => OS.GetName().ToLower();
+
+    public static bool IsDesktop()
+    {
+        return OsName == "windows" || OsName == "macos" || OsName == "linux" || OsName == "freebsd" || OsName == "bsd" || OsName == "openbsd";
+    }
+
+    public static bool IsMobile()
+    {
+        return OsName == "android" || OsName == "ios";
+    }
+}
