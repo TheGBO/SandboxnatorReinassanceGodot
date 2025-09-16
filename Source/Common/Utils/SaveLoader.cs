5c865edb7a17dde36b016b5380206360e34f88f1
NullCyan <gabrielthejota@gmail.com>
2025-09-16 15:20:42 -0300

    Add SaveLoader.cs

--- Source/Common/Utils/SaveLoader.cs
+++ Source/Common/Utils/SaveLoader.cs
@@ -2,7 +2,27 @@ using Godot;
 using System;
 namespace NullCyan.Util;
 
-public partial class SaveLoader : Node
+public partial class SaveLoader : Singleton<SaveLoader>
 {
+    public string SavePath => SetupGameSavePath();
 
+    public override void _Ready()
+    {
+        SetupGameSavePath();
+        GD.PrintRich($"[color=RED]{SavePath}[/color]");
+    }
+
+    public string SetupGameSavePath()
+    {
+        string pathRoot = "";
+        if (PlatformCheck.IsDesktop())
+        {
+            pathRoot = OS.GetExecutablePath();
+        }
+        else
+        {
+            pathRoot = OS.GetUserDataDir();
+        }
+        return $"{pathRoot}/sandboxnator{ProjectSettings.GetSetting("application/config/version").ToString().Replace(".","_")}/";
+    }
 }
