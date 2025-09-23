f2be52b7994507f54eecf1ad3c723fb33896f337
NullCyan <gabrielthejota@gmail.com>
2025-09-23 16:42:26 -0300

    Make a common ResourceIO class to handle dynamically loading resources.

--- GameContent/Registry/ItemRegistryManager.cs
+++ GameContent/Registry/ItemRegistryManager.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using NullCyan.Sandboxnator.Registry;
 using NullCyan.Util.Log;
+using NullCyan.Util.IO;
 namespace NullCyan.Sandboxnator.Item;
 
 /// <summary>
@@ -14,7 +15,7 @@ namespace NullCyan.Sandboxnator.Item;
 
 public partial class ItemRegistryManager : IRegistryManager
 {
-	/// TODO: Make these functionalities for loading and auto registering game assets generic and type agnostic
+	/// PROGRESS: Make these functionalities for loading and auto registering game assets generic and type agnostic
 	private string itemContentsPath = "res://GameContent/Items";
 
 	/// <summary>
@@ -22,83 +23,23 @@ public partial class ItemRegistryManager : IRegistryManager
 	/// </summary>
 	public void Register()
 	{
-		string path = itemContentsPath;
-		NcLogger.Log("Item content path:" + itemContentsPath, NcLogger.LogType.Register);
-		NcLogger.Log("Loading vanilla item data...", NcLogger.LogType.Register);
-		using var dir = DirAccess.Open(path);
-		if (dir != null)
+		List<Resource> itemResources = ResourceIO.GetResources<ItemData>(itemContentsPath);
+		foreach (ItemData res in itemResources)
 		{
-			string[] directories = dir.GetDirectories();
-			foreach (string itemDir in directories)
+			NcLogger.Log($"Valid item resource is {res.itemID}, registering...", NcLogger.LogType.Register);
+			BaseItem itemScene = res.itemScene.Instantiate<BaseItem>();
+			if (itemScene is PlacingItem)
 			{
-				//underscore filters out abstract items
-				if (!itemDir.StartsWith("_"))
-				{
-					string absoluteItemDir = path + "/" + itemDir;
-					List<Resource> resources = LoadItemDataResources(absoluteItemDir);
-					NcLogger.Log($"Found item resource(s) at {absoluteItemDir}:", NcLogger.LogType.Register);
-					foreach (ItemData res in resources)
-					{
-						NcLogger.Log($"Valid item resource is {res.itemID}, registering...", NcLogger.LogType.Register);
-						BaseItem itemScene = res.itemScene.Instantiate<BaseItem>();
-						if (itemScene is PlacingItem)
-						{
-							NcLogger.Log($"({res.itemID}) is a placeable building, adding to building registry as well.", NcLogger.LogType.Register);
-							GameRegistries.Instance.BuildingRegistry.Register(res.itemID, ((PlacingItem)itemScene).buildingScene);
-						}
-
-						//Register the item via resource
-						GameRegistries.Instance.ItemRegistry.Register(res.itemID, res);
-					}
-				}
+				NcLogger.Log($"({res.itemID}) is a placeable building, adding to building registry as well.", NcLogger.LogType.Register);
+				GameRegistries.Instance.BuildingRegistry.Register(res.itemID, ((PlacingItem)itemScene).buildingScene);
 			}
-		}
-		else
-		{
-			NcLogger.Log($"The path: \"{path}\" returned null on file access attempt!", NcLogger.LogType.Error);
+
+			//Register the item via resource
+			GameRegistries.Instance.ItemRegistry.Register(res.itemID, res);
 		}
 	}
 
 
-	/// <summary>
-	/// Loads the item data from game content folder in order to register them.
-	/// </summary>
-	/// <param name="path"></param>
-	/// <returns></returns>
-	private List<Resource> LoadItemDataResources(string path)
-	{
-		List<Resource> resources = new List<Resource>();
 
-		DirAccess dirAccess = DirAccess.Open(path);
-		if (dirAccess == null) { return null; }
 
-		string[] files = dirAccess.GetFiles();
-		if (files == null) { return null; }
-
-		string remapSuffix = ".remap";
-		string importSuffix = ".import";
-		string uidSuffix = ".uid";
-
-		foreach (string fileName in files)
-		{
-			string loadFileName = fileName;
-			if (fileName.Contains(remapSuffix))
-			{
-				NcLogger.Log($"REMAP FOUND {fileName}", NcLogger.LogType.Register);
-				loadFileName = StringUtils.TrimSuffix(fileName, remapSuffix);
-			}
-
-			if (!fileName.Contains(importSuffix) && !fileName.Contains(uidSuffix))
-			{
-				string resPath = path + "/" + loadFileName;
-				Resource loadedRes = GD.Load<Resource>(resPath);
-				if (loadedRes.GetType() == typeof(ItemData))
-				{
-					resources.Add(loadedRes);
-				}
-			}
-		}
-
-		return resources;
-	}
 }
---
+++ Source/Common/Utils/IO/ResourceIO.cs
@@ -0,0 +1,86 @@
+using System.Collections.Generic;
+using Godot;
+using NullCyan.Sandboxnator.Registry;
+using NullCyan.Util.Log;
+
+namespace NullCyan.Util.IO;
+
+public class ResourceIO
+{
+
+    /// <summary>
+    /// Loads the item data from game content folder in order to register them.
+    /// </summary>
+    /// <param name="path"></param>
+    /// <returns></returns>
+    private static List<Resource> HandleResourcePath<T>(string path)
+    {
+        List<Resource> resources = new List<Resource>();
+
+        DirAccess dirAccess = DirAccess.Open(path);
+        if (dirAccess == null) { return null; }
+
+        string[] files = dirAccess.GetFiles();
+        if (files == null) { return null; }
+
+        string remapSuffix = ".remap";
+        string importSuffix = ".import";
+        string uidSuffix = ".uid";
+
+        foreach (string fileName in files)
+        {
+            string loadFileName = fileName;
+            if (fileName.Contains(remapSuffix))
+            {
+                NcLogger.Log($"REMAP FOUND {fileName}", NcLogger.LogType.Register);
+                loadFileName = StringUtils.TrimSuffix(fileName, remapSuffix);
+            }
+
+            if (!fileName.Contains(importSuffix) && !fileName.Contains(uidSuffix))
+            {
+                string resPath = path + "/" + loadFileName;
+                Resource loadedRes = GD.Load<Resource>(resPath);
+                if (loadedRes.GetType() == typeof(T))
+                {
+                    resources.Add(loadedRes);
+                }
+            }
+        }
+
+        return resources;
+    }
+
+    public static List<Resource> GetResources<T>(string contentPath)
+    {
+        List<Resource> totalResources = new();
+        string path = contentPath;
+		NcLogger.Log("Item content path:" + contentPath, NcLogger.LogType.Register);
+		NcLogger.Log("Loading vanilla item data...", NcLogger.LogType.Register);
+		using var dir = DirAccess.Open(path);
+        if (dir != null)
+        {
+            string[] directories = dir.GetDirectories();
+            //Go inside each directorie for a deeper search regarding resources
+            foreach (string resDirectory in directories)
+            {
+                //underscore filters out abstract items
+                if (!resDirectory.StartsWith("_"))
+                {
+                    string absoluteResDirectory = path + "/" + resDirectory;
+                    List<Resource> currentLocalResources = HandleResourcePath<T>(absoluteResDirectory);
+                    NcLogger.Log($"Found {typeof(T).Name} resource(s) at {absoluteResDirectory}:", NcLogger.LogType.Register);
+                    foreach (Resource res in currentLocalResources)
+                    {
+                        totalResources.Add(res);
+                    }
+                }
+            }
+            return totalResources;
+        }
+        else
+        {
+            NcLogger.Log($"The path: \"{path}\" returned null on file access attempt!", NcLogger.LogType.Error);
+        }
+        return null;
+    }
+}
